0988af659135455f2b2b9eaf7b8e7aa188fc26a3
Jeremay <t-jeremy@hotmail.fr>
2019-12-01 21:25:58 +0100

    Ajout de MVC + lancement de l'IHM

---
+++ src/Cplex.java
@@ -0,0 +1,10 @@
+import java.io.File;
+import java.util.ArrayList;
+
+public class Cplex {
+    private File modele;
+    public ArrayList<Integer> executer(ArrayList<Integer> entry){
+        // TODO
+        return entry;
+    }
+}
