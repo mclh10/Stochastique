f25a87b7e232bf709f32d87837bc61e80bdb8ebe
mclh10 <homylinh95@gmail.com>
2019-12-07 23:10:09 +0100

    test MLH

--- src/RecuitDeterministe.java
+++ src/RecuitDeterministe.java
@@ -3,10 +3,12 @@ import java.util.HashMap;
 
 //Ici recuit appliqué à un seul scénario
 public class RecuitDeterministe extends Recuit {
+
+    protected static HashMap<Station, Integer> myHmMeilleure = new HashMap<Station, Integer>();
+
     @Override
     HashMap<Station,Integer> executer() {
-        //TODO
-        return null;
+        return myHmMeilleure;
     }
     //Constructeur
     public RecuitDeterministe(float myTemp, int myNbIt, int myPalierTemp, ArrayList<Scenario> myArrayScenario) {
@@ -18,24 +20,24 @@ public class RecuitDeterministe extends Recuit {
         //myHm = ensemble des solutions initiales d'un scenario donné
         //Variable de retour
         HashMap<Station, Integer> myHmCourante = new HashMap<Station, Integer>();
-        HashMap<Station, Integer> myHmMeilleure = new HashMap<Station, Integer>();
+        //HashMap<Station, Integer> myHmMeilleure = new HashMap<Station, Integer>();
         HashMap<Station, Integer> myHmVoisinage = new HashMap<Station, Integer>();
         myHmCourante = myHm;
-        myHmMeilleure = myHmMeilleure;
+        myHmMeilleure = myHmCourante;
 
         //Initialisation temp et nb_iterations se fait deja a la creation du recuit
         //Ci-dessous choper les stations
         //Variables locales xMeilleur, xCourant, xPrime
         //Calcul de la fonction objectif
         float fMin = this.getMonProbleme().calculFctObjectif(myHmMeilleure);
-        float fCourant = this.getMonProbleme().calculFctObjectif(myHmCourante);
         while (this.getTemp() >= this.getPalierTemperature()){
             while(getNbIterations() >= 0){
                 //Definir le voisinage a ce moment la
                 myHmVoisinage = this.calcVoisin(myHm);
                 //Calcul delta fonction objectif
-                //Besoin de changer la définition de la fonction Objectif paramètre --> ArrayList<Integer> a Integer
-                float delta =  this.getMonProbleme().calculFctObjectif(myHmVoisinage) - this.getMonProbleme().calculFctObjectif(myHmCourante);
+                float fCourant = this.getMonProbleme().calculFctObjectif(myHmCourante);
+                float fVoisinage = this.getMonProbleme().calculFctObjectif(myHmVoisinage);
+                float delta =  fVoisinage - fCourant;
                 if (delta < 0){
                     myHmCourante = myHmVoisinage;
                     setNbIterations(getNbIterations()-1);
@@ -44,11 +46,12 @@ public class RecuitDeterministe extends Recuit {
                         myHmMeilleure = myHmCourante;
                         break;
                     }
-                    else{
-                        if (calculerCstGibbsBoltz(-delta, getTemp())){
-                            //Xi = Xj ? What
-                            setNbIterations(getNbIterations()-1);
-                        }
+                }
+                else{
+                    if (calculerCstGibbsBoltz(-delta, getTemp())){
+                        //Xi = Xj ? What
+                        myHmCourante = myHmVoisinage;
+                        setNbIterations(getNbIterations()-1);
                     }
                 }
             }
@@ -57,6 +60,5 @@ public class RecuitDeterministe extends Recuit {
          }
         //Retour de la meilleure solution avec myHmMeilleure
         return myHmMeilleure;
-
     }
 }
