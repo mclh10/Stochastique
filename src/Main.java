085fbbf19a0f5b500af46fea06e9022a14248618
Cyan <callie847@gmail.com>
2019-12-07 20:22:07 +0100

    minor changes to main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int x =10; //nb scénarios à générer
+        System.out.println("running the program may take some time depending on the number of scenarios to generate");
         ProblemeVLS vls = new ProblemeVLS();
         vls.parseData();
+        int x =1; //nb scénarios à générer
         vls.genererScenarios(x);
         System.out.println("ça compile (OK)");
     }
