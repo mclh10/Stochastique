915d89d02e5cab160ae7864d81ba193f0c1736b8
mclh10 <homylinh95@gmail.com>
2019-12-07 22:28:15 +0100

    test MLH

--- src/Recuit.java
+++ src/Recuit.java
@@ -1,25 +1,71 @@
+import java.util.ArrayList;
 import java.util.HashMap;
-
+import java.util.Random;
 
 public abstract class Recuit extends Algorithme {
     //Variables de classe
-    private float temperature;
-    private int nbIterations;
-    private int palierTemperature;
+    private float temperature = 100f;
+    private int nbIterations = 10;
+    private int palierTemperature = 40;
+    private ArrayList<Scenario> scenarioList; //Deterministe = 1 scénario sinon plusieurs pour le stocha
 
-    //M�thodes de classe
 
-    public boolean calculerCstGibbsBoltz(int a, int b) {
-        //TODO
-        return false;
+    //Constructeur de la classe Recuit
+    public Recuit(float myTemp, int myNbIt, int myPalierTemp, ArrayList<Scenario> myScenario) {
+        this.temperature = myTemp;
+        this.nbIterations = myNbIt;
+        this.palierTemperature = myPalierTemp;
+        this.getMonProbleme().setMesScenarios(myScenario);
+    }
+    public Recuit(ArrayList<Scenario> myScenario) {
+        this.getMonProbleme().setMesScenarios(myScenario);
     }
 
-    public HashMap<Station,Integer> calcVoisin(HashMap<Station,Integer> array) {
-        //TODO
-        return null;
+    //Methodes de classe
+    // True si p <= exp(-deltaF/temperature)
+    public boolean calculerCstGibbsBoltz(double myDeltaF, float myTemperature) {
+        //Tirer une probabilité au hasard, nombre compris entre 0 et 1
+        double myProba = Math.random() ;
+        //Calcul constante de Gibbs-Boltzmann
+        double myGB = Math.exp(-myDeltaF)/myTemperature;
+        return myProba <= myGB;
     }
 
-    public HashMap<Station,Integer> recuit(HashMap<Station,Integer> array ){
+
+    public HashMap<Station,Integer> calcVoisin(HashMap<Station,Integer> myHm) {
+        //HashMap renvoyée en résultat
+        HashMap<Station, Integer> myHmResultat = new HashMap<Station, Integer>(myHm);
+        //Probabilite de tirer un nombre entre 0 et 1
+        double probaAction = Math.random();
+        //Tirer une station au hasard
+        Random generator = new Random();
+        Station[] values = (Station[]) myHm.keySet().toArray();
+        Station stationAffectee = (Station) values[generator.nextInt(values.length)];
+
+        //3 actions possibles au niveau du voisinage
+        //Si 0 < probaAction < 0,33, ajouter un vélo
+        if(probaAction >= 0 && probaAction < 0.33) {
+            //Prend une station au hasard
+            myHmResultat.put(stationAffectee, myHmResultat.get(stationAffectee)+1);
+        }
+        //Si 0,33 < probaAction < 0,66 enlever un vélo
+        else if(probaAction >= 0.33f && probaAction < 0.66){
+            myHmResultat.put(stationAffectee, myHmResultat.get(stationAffectee)-1);
+        }
+        //Si 0,66 < probaAction < 1 tranférer d'une station A à une station B
+        else{
+            //Tirage d'une deuxieme station qui interviendra dans le transfert d'un vélo
+            Station stationAffectee2 = (Station) values[generator.nextInt(values.length)];
+            //La première station a un vélo qui sera enlevé
+            myHmResultat.put(stationAffectee, myHmResultat.get(stationAffectee)-1);
+            //La deuxième station a un vélo qui sera ajouté
+            myHmResultat.put(stationAffectee2, myHmResultat.get(stationAffectee2)+1);
+        }
+        return myHmResultat;
+    }
+
+    //Renvoie HashMap<Station,Integer> array
+    public HashMap<Station,Integer> runRecuit(HashMap<Station,Integer> array ){
         //TODO
         return null;
     }
@@ -32,12 +78,19 @@ public abstract class Recuit extends Algorithme {
         return this.nbIterations;
     }
 
-    public void setTemp(int newTemp) {
+    public void setTemp(float newTemp) {
         this.temperature = newTemp;
     }
 
     public void setNbIterations(int newNbIt) {
         this.nbIterations = newNbIt;
+    }
+
+    public int getPalierTemperature(){
+        return this.palierTemperature;
+    }
+    public void setPalierTemperature(int myPalierTemp){
+        this.palierTemperature = myPalierTemp;
     }
 
 }
--- src/RecuitDeterministe.java
+++ src/RecuitDeterministe.java
@@ -1,10 +1,62 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 
+//Ici recuit appliqué à un seul scénario
 public class RecuitDeterministe extends Recuit {
     @Override
     HashMap<Station,Integer> executer() {
         //TODO
         return null;
     }
+    //Constructeur
+    public RecuitDeterministe(float myTemp, int myNbIt, int myPalierTemp, ArrayList<Scenario> myArrayScenario) {
+        super(myTemp, myNbIt, myPalierTemp, myArrayScenario);
+    }
 
+    @Override
+    public HashMap<Station,Integer>  runRecuit(HashMap<Station,Integer> myHm){
+        //myHm = ensemble des solutions initiales d'un scenario donné
+        //Variable de retour
+        HashMap<Station, Integer> myHmCourante = new HashMap<Station, Integer>();
+        HashMap<Station, Integer> myHmMeilleure = new HashMap<Station, Integer>();
+        HashMap<Station, Integer> myHmVoisinage = new HashMap<Station, Integer>();
+        myHmCourante = myHm;
+        myHmMeilleure = myHmMeilleure;
+
+        //Initialisation temp et nb_iterations se fait deja a la creation du recuit
+        //Ci-dessous choper les stations
+        //Variables locales xMeilleur, xCourant, xPrime
+        //Calcul de la fonction objectif
+        float fMin = this.getMonProbleme().calculFctObjectif(myHmMeilleure);
+        float fCourant = this.getMonProbleme().calculFctObjectif(myHmCourante);
+        while (this.getTemp() >= this.getPalierTemperature()){
+            while(getNbIterations() >= 0){
+                //Definir le voisinage a ce moment la
+                myHmVoisinage = this.calcVoisin(myHm);
+                //Calcul delta fonction objectif
+                //Besoin de changer la définition de la fonction Objectif paramètre --> ArrayList<Integer> a Integer
+                float delta =  this.getMonProbleme().calculFctObjectif(myHmVoisinage) - this.getMonProbleme().calculFctObjectif(myHmCourante);
+                if (delta < 0){
+                    myHmCourante = myHmVoisinage;
+                    setNbIterations(getNbIterations()-1);
+                    if(fCourant < fMin){
+                        fMin = fCourant;
+                        myHmMeilleure = myHmCourante;
+                        break;
+                    }
+                    else{
+                        if (calculerCstGibbsBoltz(-delta, getTemp())){
+                            //Xi = Xj ? What
+                            setNbIterations(getNbIterations()-1);
+                        }
+                    }
+                }
+            }
+            //fonction décroissante de la température
+            setTemp(getTemp()-1f);
+         }
+        //Retour de la meilleure solution avec myHmMeilleure
+        return myHmMeilleure;
+
+    }
 }
