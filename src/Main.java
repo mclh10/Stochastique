cdf1a533b94a4c3df66c3959b032533cfb64a4e0
Cyan <callie847@gmail.com>
2019-12-08 22:11:29 +0100

    implemented fctObjSousRecuit

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,19 @@ public class Main {
         int x =1; //nb scénarios à générer
         vls.genererScenarios(x);
         HashMap<Station,Integer> sol = new HashMap<>();
+        HashMap<Station,Integer> phi = new HashMap<>();
         for(Station s : vls.getMesStations()){
             sol.put(s,0);
+            phi.put(s,1);
         }
         if(vls.verifierContraintes(sol)){
             System.out.println(vls.calculFctObjectif(sol));
         }
+        System.out.println(vls.calculFctObjectif(sol));
+        HashMap<Scenario,HashMap<Station, Integer>> lambda = new HashMap<>();
+        Scenario scen =vls.getMesScenarios().get(0);
+        lambda.put(scen,phi);
+        System.out.println(vls.calculFctObjSousRecuit(scen,sol,lambda,phi,sol));
         System.out.println("ça compile (OK)");
     }
 }
--- src/ProblemeVLS.java
+++ src/ProblemeVLS.java
@@ -71,11 +71,12 @@ public class ProblemeVLS extends Probleme {
                     Station j = (Station) me2.getKey();
                     int beta_ijs = scenario.getBeta().get(s).get(j);
                     int xi_ijs = scenario.getDonnees().get(s).get(j);
-                    int Imoins = beta_ijs - x_i; //>0 ? beta_ijs -x_i :0;
-                    if (beta_ijs != xi_ijs - Imoins) { //contrainte 1b
+                    int Imoins = beta_ijs - x_i ;//>0 ? beta_ijs -x_i :0;
+                    /*if (beta_ijs != xi_ijs - Imoins) { //contrainte 1b
                         //System.out.println("2");
+                        System.out.println(beta_ijs +","+xi_ijs+","+Imoins);
                         return false;
-                    }
+                    }*/
                     sumImoins+=Imoins;
                     sumXi+=xi_ijs;
                     sumBeta+=beta_ijs;
@@ -94,6 +95,65 @@ public class ProblemeVLS extends Probleme {
     @Override
     public float calculFctObjectif(HashMap<Station,Integer> currentSolution) {
         int sommeCX=0;
+        for(Map.Entry me : currentSolution.entrySet()) {
+            Station s = (Station) me.getKey();
+            int x_i = (int) me.getValue();
+            sommeCX += s.getCi() * s.getXi();
+        }
+        int sommeScenar=0;
+        for(Scenario sce : this.mesScenarios) {
+            int sommeAcc=0;
+            for(Map.Entry me : currentSolution.entrySet()) {
+                Station s = (Station) me.getKey();
+                int x_i = (int) me.getValue();
+                int Imoins = 0;
+                int Omoins = x_i - s.getKi();
+                int sumBeta_ijs=0;
+                int sumBeta_jis=0;
+                for(Map.Entry me2 : sce.getDonnees().entrySet()){
+                    Station j = (Station) me.getKey();
+                    int beta_ijs = sce.getBeta().get(s).get(j);
+                    //calcul de la somme beta_ijs
+                    sumBeta_ijs += beta_ijs;
+                    //calcul de la somme des beta_jis
+                    sumBeta_jis = sce.getBeta().get(j).get(s);
+                    //calcul de la somme des I_ijs_moins
+                    Imoins += beta_ijs - x_i>0 ? beta_ijs - x_i : 0;
+                }
+                //calcul de O_is_moins
+                Omoins += sumBeta_ijs - sumBeta_jis;
+                Omoins = Omoins > 0 ? Omoins : 0;
+                sommeAcc += s.getVi()*Imoins+s.getWi()*Omoins;
+            }
+            sommeScenar += sce.getProba()*sommeAcc;
+        }
+        return sommeCX+sommeScenar;
+    }
+
+
+    public float calculFctObjSousRecuit(Scenario scenario, HashMap<Station,Integer> currentSolution, HashMap<Scenario,HashMap<Station,Integer>> lambda, HashMap<Station,Integer> phi, HashMap<Station,Integer> xref) {
+        int sum1 = 0;
+        int sum2 = 0;
+        int sum3 = 0;
+        for(Map.Entry me : currentSolution.entrySet()){
+            Station i = (Station) me.getKey();
+            int x_i = (int) me.getValue();
+            sum1+=(i.getCi()+lambda.get(scenario).get(i)-phi.get(i)*xref.get(i))*x_i;
+            sum2+=x_i*x_i;
+            int sumImoins = 0;
+            int beta_ijs = 0;
+            int beta_jis = 0;
+            for(Map.Entry me2 : currentSolution.entrySet()){
+                Station j = (Station) me.getKey();
+                sumImoins+=scenario.getBeta().get(i).get(j) - x_i > 0 ? scenario.getBeta().get(i).get(j) : 0;
+                beta_ijs += scenario.getBeta().get(i).get(j);
+                beta_jis += scenario.getBeta().get(j).get(i);
+            }
+            int oMoins = beta_ijs - i.getKi() + x_i + beta_jis > 0 ? beta_ijs - i.getKi() + x_i + beta_jis : 0;
+            sum3 += i.getVi()*sumImoins+i.getWi()*oMoins;
+        }
+        return (sum1 + sum2/2 + sum3);
+        /*int sommeCX=0;
         for(Map.Entry me : currentSolution.entrySet()) {
             Station s = (Station) me.getKey();
             int x_i = (int) me.getValue();
@@ -126,10 +186,9 @@ public class ProblemeVLS extends Probleme {
             }
             sommeScenar += sce.getProba()*sommeAcc;
         }
-        return sommeCX+sommeScenar;
+        return sommeCX+sommeScenar;*/
     }
 
-    //Méthodes de la classe
 
     //parsing du fichier JSON récupéré sur opendata.paris.fr
     public void parseData(){
@@ -218,6 +277,7 @@ public class ProblemeVLS extends Probleme {
             Scenario sce = new Scenario(donneesScenario,betaScenar2,new HashMap<>(),proba);
             listeScenarios.add(sce);
         }
+        this.setMesScenarios(listeScenarios);
         return listeScenarios;
     }
 }
