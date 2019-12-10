b1ba4c02c64d907af0fae287d3d89a0298971550
Cyan <callie847@gmail.com>
2019-12-10 21:36:05 +0100

    minor changes to IHM

--- src/com/stocha/graphic/Controller.java
+++ src/com/stocha/graphic/Controller.java
@@ -3,10 +3,7 @@ package com.stocha.graphic;
 import com.stocha.ProblemeVLS;
 import com.stocha.Station;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Spinner;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 
 import java.util.HashMap;
@@ -36,11 +33,15 @@ public class Controller {
     Button calculateButton;
     @FXML
     TextArea resultTextArea;
+    @FXML
+    TextField filePath;
 
     @FXML
     public void initialize() {
         algoComboBox.getItems().addAll(Algo.values());
+        filePath.setText("velib-disponibilite-en-temps reel.json");
         calculateButton.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> run());
+
     }
 
     public void testFunction(){
@@ -48,30 +49,35 @@ public class Controller {
     }
 
     private void run(){
-        calculateButton.setDisable(true);
-        resultTextArea.setText("The calculation may take some time");
-        new Thread(() -> {
-            ProblemeVLS vls = new ProblemeVLS();
-            vls.parseData();
-            int x =stationNumberSpinner.getValue(); //nb scénarios à générer
-            vls.genererScenarios(x);
-            HashMap<Station,Integer> sol = new HashMap<>();
-            for(Station s : vls.getMesStations()){
-                sol.put(s,0);
-            }
+        if(algoComboBox.getValue()!= null ) {
+            calculateButton.setDisable(true);
+            resultTextArea.setText("The calculation may take some time");
+            new Thread(() -> {
+                ProblemeVLS vls = new ProblemeVLS();
+                vls.parseData();
+                int x = stationNumberSpinner.getValue(); //nb scénarios à générer
+                vls.genererScenarios(x);
+                HashMap<Station, Integer> sol = new HashMap<>();
+                for (Station s : vls.getMesStations()) {
+                    sol.put(s, 0);
+                }
 
-            switch(algoComboBox.getValue()){
-                case CPLEX:
-                    break;
-                case RECUITSTO:
-                    break;
-                case RECUITDETER:
-                    break;
-            }
-            float res = vls.calculFctObjectif(sol);
-            resultTextArea.setText("résultat fonction objectif : " + res);
-            calculateButton.setDisable(false);
-        }).start();
+                switch (algoComboBox.getValue()) {
+                    case CPLEX:
+                        break;
+                    case RECUITSTO:
+                        break;
+                    case RECUITDETER:
+                        break;
+                }
+                float res = vls.calculFctObjectif(sol);
+                resultTextArea.setText("résultat fonction objectif : " + res);
+                calculateButton.setDisable(false);
+            }).start();
+        }
+        else{
+            resultTextArea.setText("Some fields are empty or incorrect, the calculation cannot be done");
+        }
     }
 
 }
