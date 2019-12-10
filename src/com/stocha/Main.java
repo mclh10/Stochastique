bf87d9ad277bda6f38a50c580beacd82fd4ff07b
Cyan <callie847@gmail.com>
2019-12-10 20:12:47 +0100

    merging

--- src/com/stocha/Main.java
+++ src/com/stocha/Main.java
@@ -1,5 +1,6 @@
 package com.stocha;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Main {
@@ -10,20 +11,59 @@ public class Main {
         int x =1; //nb scénarios à générer
         vls.genererScenarios(x);
         HashMap<Station,Integer> sol = new HashMap<>();
-        HashMap<Station,Integer> phi = new HashMap<>();
         for(Station s : vls.getMesStations()){
             sol.put(s,0);
-            phi.put(s,1);
         }
         if(vls.verifierContraintes(sol)){
             System.out.println(vls.calculFctObjectif(sol));
         }
-        System.out.println(vls.calculFctObjectif(sol));
-        HashMap<Scenario,HashMap<Station, Integer>> lambda = new HashMap<>();
-        Scenario scen =vls.getMesScenarios().get(0);
-        lambda.put(scen,phi);
-        System.out.println(vls.calculFctObjSousRecuit(scen,sol,lambda,phi,sol));
-        //System.out.println(vls.calculFctObjGenerale(sol,lambda,phi,sol));
+
+        //Initialisation des scenarios
+        ArrayList<Scenario> myScenarioList = new ArrayList<Scenario>(vls.getMesScenarios());
+        //Initialisation du recuitDeter
+        RecuitDeterministe recuitDeter = new RecuitDeterministe(10, 2, 8, myScenarioList, false );
+
+        //recuitDeter.runRecuit(vls.getMesStations().get(0).getXiij());
+
         System.out.println("ça compile (OK)");
+
+
+
+        // parsing the vls problem into cplex
+
+        int n = vls.getMesStations().size(); // number of station
+        double[] k = new double[n]; // slot by station
+        double[] c = new double[n]; // cost of a bike by station
+        double[] v = new double[n]; // cost if 0 bike at station i
+        double[] w = new double[n]; // time cost for the user if station i is full
+        double[][] xsi = new double[n][n]; // demand from station i to j
+
+
+        // initializing xsi to 0 in case the parsing fails
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 0; j < n; j++) {
+//                xsi[i][j] = 0;
+//            }
+//
+//        }
+
+        // parsing the vls problem into the variables for cplex
+        int i = 0;
+        for(Station s : vls.getMesStations()){
+            k[i] = s.getKi();
+            c[i] = s.getCi();
+            int j = 0;
+            for(Station s2 : vls.getMesStations()){
+                xsi[i][j]= (double) s.getXiij().get(s2);
+                j++;
+            }
+            i++;
+        }
+
+
+
+        // solving the problem with cplex
+        Cplex.solve(n, k, c, v, w, xsi);
+
     }
 }
--- src/com/stocha/RecuitDeterministe.java
+++ src/com/stocha/RecuitDeterministe.java
@@ -1,45 +1,73 @@
 package com.stocha;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 
 //Ici recuit appliqué à un seul scénario
 public class RecuitDeterministe extends Recuit {
+    //Variables
+    protected static HashMap<Station, Integer> myHmMeilleure;
+    private boolean estSousRecuit;
+    private HashMap<Station, Float> lambdaDeter;
+    private HashMap<Station, Float> phiDeter;
 
-    protected static HashMap<Station, Integer> myHmMeilleure = new HashMap<Station, Integer>();
-
+    //Mes methodes ici
     @Override
     HashMap<Station,Integer> executer() {
+        //TODO faut-il modifier les paramètres de la méthode runRecuit pour juste get ma variable myHmmeilleure ici
         return myHmMeilleure;
     }
     //Constructeur
-    public RecuitDeterministe(float myTemp, int myNbIt, int myPalierTemp, ArrayList<Scenario> myArrayScenario) {
+    public RecuitDeterministe(float myTemp, int myNbIt, int myPalierTemp, ArrayList<Scenario> myArrayScenario, boolean myEstSousRecuit) {
         super(myTemp, myNbIt, myPalierTemp, myArrayScenario);
+        this.estSousRecuit = myEstSousRecuit;
     }
+
+
 
     @Override
     public HashMap<Station,Integer>  runRecuit(HashMap<Station,Integer> myHm){
         //myHm = ensemble des solutions initiales d'un scenario donné
         //Variable de retour
-        HashMap<Station, Integer> myHmCourante = new HashMap<Station, Integer>();
-        //HashMap<com.stocha.Station, Integer> myHmMeilleure = new HashMap<com.stocha.Station, Integer>();
-        HashMap<Station, Integer> myHmVoisinage = new HashMap<Station, Integer>();
+        HashMap<Station, Integer> myHmCourante = new HashMap<Station, Integer>(myHm);
+        myHmMeilleure = new HashMap<Station, Integer>(myHm);
+        HashMap<Station, Integer> myHmVoisinage = new HashMap<Station, Integer>(myHm);
         myHmCourante = myHm;
         myHmMeilleure = myHmCourante;
 
-        //Initialisation temp et nb_iterations se fait deja a la creation du recuit
-        //Ci-dessous choper les stations
-        //Variables locales xMeilleur, xCourant, xPrime
         //Calcul de la fonction objectif
-        float fMin = this.getMonProbleme().calculFctObjectif(myHmMeilleure);
+        float fMin, fCourant, fVoisinage, delta;
+        if (estSousRecuit){
+            //Ici mettre la fonction objective du sous recuit
+            //+ calculFctObjSousRecuit(x_i, lambda, phi, xref): float
+            //TODO Changer la fonction correspondante
+            fMin = this.getMonProbleme().calculFctObjectif(myHmMeilleure);
+        }
+        else{
+            //Ici mettre la fonction objectif du recuite général
+            //+ calculFctObjGenerale(x_i, lambda, phi, xref): float
+            //TODO Changer la fonction correspondante
+            fMin = this.getMonProbleme().calculFctObjectif(myHmMeilleure);
+        }
+
+        //Execution de l'ensemble du recuit
         while (this.getTemp() >= this.getPalierTemperature()){
             while(getNbIterations() >= 0){
                 //Definir le voisinage a ce moment la
                 myHmVoisinage = this.calcVoisin(myHm);
                 //Calcul delta fonction objectif
-                float fCourant = this.getMonProbleme().calculFctObjectif(myHmCourante);
-                float fVoisinage = this.getMonProbleme().calculFctObjectif(myHmVoisinage);
-                float delta =  fVoisinage - fCourant;
+                if (estSousRecuit){
+                    //Ici mettre la fonction objective du sous recuit
+                    //+ calculFctObjSousRecuit(x_i, lambda, phi, xref): float
+                    //TODO Changer la fonction correspondante
+                    fCourant = this.getMonProbleme().calculFctObjectif(myHmCourante);
+                    fVoisinage = this.getMonProbleme().calculFctObjectif(myHmVoisinage);
+                }
+                else{
+                    //Ici mettre la fonction objectif normale
+                    fCourant = this.getMonProbleme().calculFctObjectif(myHmCourante);
+                    fVoisinage = this.getMonProbleme().calculFctObjectif(myHmVoisinage);
+                }
+                delta =  fVoisinage - fCourant;
                 if (delta < 0){
                     myHmCourante = myHmVoisinage;
                     setNbIterations(getNbIterations()-1);
